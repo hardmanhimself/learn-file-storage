0f2259d43ab9b52bb9da84c3242f750d250e8df2
JD Hardman <josh.hardman98@gmail.com>
2025-11-23 15:31:48 +0000

    caching

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -49,7 +49,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	assetPath := getAssetPath(videoID, mediaType)
+	assetPath := getAssetPath(mediaType)
 	assetDiscPath := cfg.getAssetDiscPath(assetPath)
 
 	dst, err := os.Create(assetDiscPath)
